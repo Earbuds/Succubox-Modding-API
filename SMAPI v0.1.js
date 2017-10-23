a7736e06d932eb39b443fae28d790d7a28a648ee
Earbuds <superblobjack@hotmail.com>
2017-10-22 20:28:23 -0500

    Initial commit

---
+++ SMAPI v0.1.js
@@ -0,0 +1,12 @@
+addDrop(resourceDatatype, resourceName, amount, rarity) {
+	droptable.push(new LootDrop(
+        new ResourceUnlock(resourceDatatype, amount),
+        amount + " " + resourceName + ".", rarity));
+
+    droptablemax = 0;
+    for(var i = 0; i<droptable.length; i++){
+        droptablemax += 100.0/droptable[i].rarity;
+    }
+
+    calc_quantum();
+}
